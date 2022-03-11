5d21ede6c5dad27848822abf51a82553187442ab
Micke thai <96061901+Mickethai@users.noreply.github.com>
2022-03-11 22:04:24 +0100

    first commit

---
+++ src/main/java/projektvers2/triskelion/Triskelion.java
@@ -0,0 +1,19 @@
+package projektvers2.triskelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Triskelion {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Triskelion.class, args);
+	}
+
+}
+
+
+
+
+
---
+++ src/test/java/projektvers2/triskelion/TriskelionApplicationTests.java
@@ -0,0 +1,13 @@
+package projektvers2.triskelion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriskelionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
